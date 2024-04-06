77f2cb974e2f515c8420863779d6592b4a8c98ad
Sherkhan <sherkhan.iskender@kcell.kz>
2024-04-07 00:23:59 +0500

    fix: build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,9 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-
+  globals: {
+    process: "module",
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
